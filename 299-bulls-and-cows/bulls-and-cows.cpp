dddf34d389ddab83d0dc8b97abeff0ae66b5c388
Akhil Pandey <akhilpandey494@gmail.com>
2026-05-27 02:52:58 +0530

    Time: 63 ms (5.83%) | Memory: 9.3 MB (15.23%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<char,unordered_set<int>>mp;
+        unordered_map<char,int>mp;
         int n=secret.size();
         for(int i=0;i<n;i++){
-            mp[secret[i]].insert(i);
+            mp[secret[i]]++;
         }
         int a=0,b=0;
         for(int i=0;i<n;i++){
-            if(mp.count(guess[i])){
-                if(mp[guess[i]].count(i)){
-                    mp[guess[i]].erase(i);
-                    if(mp[guess[i]].size()==0) mp.erase(guess[i]);
-                    a++;
-                    guess[i]+='9';
-                }
+            if(guess[i]==secret[i]){
+                mp[secret[i]]--;
+                a++;
+                guess[i]+='9';
             }
         }
         for(int i=0;i<n;i++){
             cout<<guess[i]<<" ";
-            if(mp.count(guess[i])){
-                mp[guess[i]].erase(mp[guess[i]].begin());
-                if(mp[guess[i]].size()==0) mp.erase(guess[i]);
+            if(mp[guess[i]]>0){
+                mp[guess[i]]--;
                 b++;
             }
         }
