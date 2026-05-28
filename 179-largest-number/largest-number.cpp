bf8c7750a31b6eaa89b2a7e2f53ef7b4c9ae5910
Akhil Pandey <akhilpandey494@gmail.com>
2026-05-28 20:55:06 +0530

    Time: 8 ms (9.04%) | Memory: 16.2 MB (89.66%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) {
-        vector<string> arr;
-        for (int num : nums) {
-            arr.push_back(to_string(num));
+    bool compare(string &a,string &b){
+        int a1=a.size();
+        int b1=b.size();
+        int i=0;
+        while(i<min(a1,b1)){
+            if(a[i]>b[i]) return false;
+            else if(a[i]<b[i]) return true;
+            i++;
         }
-        sort(arr.begin(),arr.end(),[](string &a,string &b){
-            return a+b>b+a;
-        });
-        string ans="";
-        if (arr[0] == "0") return "0";
+        int j=i-1;
+        if(a1>b1){
+            while(i<a1){
+                if(a[i]>b[j]) return false;
+                else if(a[i]<b[j]) return true;
+                i++;
+            }
+        }
+        else if(a1<b1){
+            while(i<b1){
+                if(a[j]>b[i]) return false;
+                else if(a[j]<b[i]) return true;
+                i++;
+            }
+        }
+        return false;
+    }
+    string largestNumber(vector<int>& nums) {
+        vector<string>arr;
+        for(auto it:nums) arr.push_back(to_string(it));
+        sort(arr.begin(),arr.end(),greater<string>());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((arr[j]+arr[i])>(arr[i]+arr[j])) swap(arr[i],arr[j]);
+            }
+        }
+        string ans;
         for(auto it:arr){
+            if(ans.size()==0 && it=="0") continue;
             ans+=it;
         }
-        return ans;
+        return ans.size()==0?"0":ans;
     }
 };
