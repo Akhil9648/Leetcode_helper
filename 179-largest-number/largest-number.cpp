3b44fc5a2a361d7bd04a0a62130d515f81bef0e4
Akhil Pandey <akhilpandey494@gmail.com>
2026-05-28 20:55:26 +0530

    Time: 5 ms (33.00%) | Memory: 16.1 MB (90.13%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    bool compare(string &a,string &b){
-        int a1=a.size();
-        int b1=b.size();
-        int i=0;
-        while(i<min(a1,b1)){
-            if(a[i]>b[i]) return false;
-            else if(a[i]<b[i]) return true;
-            i++;
-        }
-        int j=i-1;
-        if(a1>b1){
-            while(i<a1){
-                if(a[i]>b[j]) return false;
-                else if(a[i]<b[j]) return true;
-                i++;
-            }
-        }
-        else if(a1<b1){
-            while(i<b1){
-                if(a[j]>b[i]) return false;
-                else if(a[j]<b[i]) return true;
-                i++;
-            }
-        }
-        return false;
-    }
     string largestNumber(vector<int>& nums) {
         vector<string>arr;
         for(auto it:nums) arr.push_back(to_string(it));
