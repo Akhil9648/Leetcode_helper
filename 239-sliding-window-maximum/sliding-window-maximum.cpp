63ee38c72091736f64da0c3b87462015700936bd
Akhil Pandey <akhilpandey494@gmail.com>
2026-05-31 19:25:33 +0530

    Time: 11 ms (97.36%) | Memory: 139.2 MB (66.63%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -6,9 +6,7 @@ public:
         vector<int>ans;
         for(int i=0;i<n;i++){
             if(!dq.empty() && dq.front()<=i-k) dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<nums[i]){
-                dq.pop_back();
-            }
+            while(!dq.empty() && nums[dq.back()]<nums[i]) dq.pop_back();
             dq.push_back(i);
             if(i>=k-1){
                 ans.push_back(nums[dq.front()]);
