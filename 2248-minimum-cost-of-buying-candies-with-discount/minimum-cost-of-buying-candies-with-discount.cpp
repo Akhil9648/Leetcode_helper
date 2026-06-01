0e8be66654d8ba52670048bf46ceb09a9993038c
Akhil Pandey <akhilpandey494@gmail.com>
2026-06-01 15:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (21.54%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,10 +4,6 @@ public:
         sort(cost.begin(),cost.end());
         int sum=0;
         int n=cost.size();
-        // if(n<=2){
-        //     for(auto it:cost) sum+=it;
-        //     return sum;
-        // }
         int i=n-1;
         for(;i>1;i-=2){
             sum+=(cost[i]+cost[i-1]);
