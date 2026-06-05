8b2e0f3e97e0c2d69810515b8b4c8cba91ba80bc
Akhil Pandey <akhilpandey494@gmail.com>
2026-06-05 22:28:42 +0530

    Time: 59 ms (25.49%) | Memory: 9.6 MB (49.01%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    int totalWaviness(int num1, int num2) {
-        auto getWaviness = [](int x) -> int {
-            string s = to_string(x);
-            int waviness = 0;
-
-            for (size_t i = 1; i < s.size() - 1; ++i) {
-                bool isPeak = s[i] > s[i - 1] && s[i] > s[i + 1];
-                bool isValley = s[i] < s[i - 1] && s[i] < s[i + 1];
-                if (isPeak || isValley) {
-                    ++waviness;
-                }
-            }
-
-            return waviness;
-        };
-
-        int total = 0;
-        for (int i = num1; i <= num2; ++i) {
-            total += getWaviness(i);
+    int count(string a){
+        int n=a.size();
+        int cnt=0;
+        for(int i=1;i<n-1;i++){
+            if((a[i]>a[i-1] && a[i]>a[i+1]) || (a[i]<a[i-1] && a[i]<a[i+1])) cnt++;
         }
-
-        return total;
+        return cnt;
+    }
+    int totalWaviness(int num1, int num2) {
+        int ans=0;
+        for(int i=num1;i<=num2;i++){
+            string a=to_string(i);
+            ans+=count(a);
+        }
+        return ans;
     }
 };
