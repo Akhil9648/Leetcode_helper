e95068ea205bb8f1795652a97d08a89319d3d093
Akhil Pandey <akhilpandey494@gmail.com>
2026-06-14 16:38:42 +0530

    Time: 101 ms (19.61%) | Memory: 348.6 MB (76.01%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.cpp
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.cpp
@@ -10,47 +10,47 @@
  */
 class Solution {
 public:
-    pair<ListNode*,ListNode*> reverse(ListNode* start,ListNode* end){
-        ListNode* ptr=start,*nxt=NULL;
-        ListNode* rev=NULL;
-        while(ptr!=end){
-            nxt=ptr->next;
-            ptr->next=rev;
-            rev=ptr;
+    ListNode* reverse(ListNode* head){
+        ListNode* ptr=head,*prev=NULL;
+        while(ptr){
+            ListNode* nxt=ptr->next;
+            ptr->next=prev;
+            prev=ptr;
             ptr=nxt;
         }
-        return {start,rev};
+        return prev;
     }
     ListNode* reverseEvenLengthGroups(ListNode* head) {
-        ListNode dummy(0);
-        dummy.next=head;
-        ListNode* temp=head,*start=&dummy;
-        int n=0,s=1;
+        ListNode* temp=head,*start=head,*prev=NULL;
+        int s=1,n=0;
         while(temp){
-            temp=temp->next;
             n++;
             if(n==s){
+                cout<<n<<" "<<s<<endl;
                 if(n%2==0){
-                   pair<ListNode*,ListNode*> a=reverse(start->next,temp);
-                    start->next=a.second;
-                    a.first->next=temp;
-                    start=a.first;
+                    ListNode* nxt=temp->next;
+                    temp->next=NULL;
+                    temp=reverse(start);
+                    prev->next=temp;
+                    start->next=nxt;
+                    temp=start;
+                    prev=temp;
+                    start=nxt;
                 }
                 else{
-                    while(n-- && start->next){
-                        start = start->next;
-                    }
+                    prev=temp;
+                    start=temp->next;
                 }
+                cout<<"val"<<"  "<<temp->val<<endl;
                 n=0;
                 s++;
-                temp=start->next;
             }
+            temp=temp->next;
         }
-        if(n > 0 && n % 2 == 0){
-            pair<ListNode*,ListNode*> a=reverse(start->next,temp);
-            start->next=a.second;
-            a.first->next=temp;
+        if(n%2==0){
+            temp=reverse(start);
+            prev->next=temp;
         }
-        return dummy.next;
+        return head;
     }
 };
