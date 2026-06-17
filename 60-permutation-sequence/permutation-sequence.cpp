f61dbccbb090e02a22de9648344a9b2fbf25b938
Akhil Pandey <akhilpandey494@gmail.com>
2026-06-18 02:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (15.71%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     string getPermutation(int n, int k) {
         vector<int>s;
@@ -10,5 +10,27 @@ public:
         for(auto it:s) ans+=to_string(it);
         return ans;
         // return s;
+    }
+};
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int>nums;
+        int fact=1;
+        for(int i=1;i<n;i++){
+            nums.push_back(i);
+            fact=fact*i;
+        }
+        nums.push_back(n);
+        k--;
+        string s;
+        while(1){
+            s+=to_string(nums[k/fact]);
+            nums.erase(nums.begin()+k/fact);
+            if(nums.size()==0) break;
+            k%=fact;
+            fact/=nums.size();
+        }
+        return s;
     }
 };
