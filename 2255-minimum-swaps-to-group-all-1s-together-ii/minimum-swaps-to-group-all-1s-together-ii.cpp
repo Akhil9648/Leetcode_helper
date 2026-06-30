37db17f96ef2f2582c7f907c764b143ee9fd3c50
Akhil Pandey <akhilpandey494@gmail.com>
2026-06-30 20:00:40 +0530

    Time: 4 ms (76.30%) | Memory: 84.3 MB (51.95%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int sum=0,maxi=0;
-        int i=0,j=0;
         int n=nums.size();
-        int cnt=0;
+        int a=0;
         for(auto it:nums){
-            if(it==1) cnt++;
+            a+=it;
         }
-        while(j<cnt){
-            sum+=nums[j];
+        int i=0,curr=0,maxi=0;
+        for(;i<a;i++){
+            curr+=nums[i];
+        }
+        int j=0;
+        maxi=max(maxi,curr);
+        for(int i=a;i<2*n;i++){
+            curr-=nums[j%n];   
             j++;
+            curr+=nums[i%n];
+            maxi=max(maxi,curr);
         }
-        if(cnt==n) return 0;
-        maxi=max(maxi,sum);
-        while(i<n){
-            sum-=nums[i];
-            sum+=nums[j];
-            i++;
-            j=(j+1)%n;
-            maxi=max(maxi,sum);
-        }
-        return cnt-maxi;
+        return a-maxi;
     }
 };
