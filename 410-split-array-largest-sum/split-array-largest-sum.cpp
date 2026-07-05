feb060860cbae0ee0b3d63eca2a5870814c134a5
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-05 15:56:08 +0530

    Time: 262 ms (3.46%) | Memory: 11.9 MB (6.35%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -7,6 +7,31 @@ public:
         }
         if(i>=n) return 1e9;
         if(dp[i][k]!=-1) return dp[i][k];
+        int curr=0;
+        int maxi=1e9;
+        for(int j=i;j<=n-k;j++){
+            curr+=nums[j];
+            int remain=solve(j+1,nums,k-1,dp);
+            int mini=max(curr,remain);
+            maxi=min(maxi,mini);
+        }
+        return dp[i][k]=maxi;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        return solve(0,nums,k,dp);
+    }
+};
+class Solution1 {
+public:
+    int n;
+    int solve(int i,vector<int>& nums,int k,vector<vector<int>>& dp){
+        if(k==0){
+            return (i>=n)?0:1e9;
+        }
+        if(i>=n) return 1e9;
+        if(dp[i][k]!=-1) return dp[i][k];
         int maxi=1e9;
         int curr=0;
         for(int j=i;j<=n-k;j++){
