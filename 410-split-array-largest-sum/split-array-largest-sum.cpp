5cf2a6addfcbfee89554ab045163a42851a62c90
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-05 15:52:42 +0530

    Time: 259 ms (3.46%) | Memory: 12.1 MB (6.35%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,62 +1,25 @@
-class Solution1 {
-public:
-    int n;
-    int solve(int i,vector<int>& nums,int k,int sum,vector<int>& arr){
-        if(i>=n){
-            arr.push_back(sum);
-            int a=*max_element(arr.begin(),arr.end());
-            arr.pop_back();
-            return a;
-        }
-        int take=INT_MAX;
-        if(k>1){
-            arr.push_back(sum);
-            take=solve(i+1,nums,k-1,nums[i],arr);
-            arr.pop_back();
-        }
-        int notTake=solve(i+1,nums,k,sum+nums[i],arr);
-        return min(take,notTake);
-    }
-    int splitArray(vector<int>& nums, int k) {
-        vector<int>arr;
-        n=nums.size();
-        return solve(1,nums,k,nums[0],arr);
-    }
-};
 class Solution {
 public:
-    bool canSolve(vector<int>& nums,int k,int mid){
-        int sum=0,cnt=1;
-        for(int i:nums){
-            if(sum+i<=mid){
-                sum+=i;
-            }
-            else{
-                sum=i;
-                cnt++;
-            }
-            if(cnt>k) return false;
+    int n;
+    int solve(int i,vector<int>& nums,int k,vector<vector<int>>& dp){
+        if(k==0){
+            return (i>=n)?0:1e9;
         }
-        return cnt<=k;
+        if(i>=n) return 1e9;
+        if(dp[i][k]!=-1) return dp[i][k];
+        int maxi=1e9;
+        int curr=0;
+        for(int j=i;j<=n-k;j++){
+            curr+=nums[j];
+            int remain=solve(j+1,nums,k-1,dp);
+            int mini=max(curr,remain);
+            maxi=min(maxi,mini);
+        }
+        return dp[i][k]=maxi;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        int low=*max_element(nums.begin(),nums.end());
-        int high=0;
-        int ans=0;
-        for(int i:nums){
-            high+=i;
-        }
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(canSolve(nums,k,mid)){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return ans;
-    }   
+        n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        return solve(0,nums,k,dp);
+    }
 };
