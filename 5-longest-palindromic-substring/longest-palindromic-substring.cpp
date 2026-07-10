2b869b69924d4930b7a7d30226dde4f0f4f8ed69
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-10 10:42:59 +0530

    Time: 2129 ms (5.01%) | Memory: 293.9 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     string ans;
-    bool isPal(int i,int j,string &s){
-        int n=s.size();
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+    bool isPal(int start,int end,string &s){
+        while(start<=end){
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
         }
         return true;
     }
-    int solve(int i,int j,string &s,vector<vector<int>>& dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j] && isPal(i,j,s)){
-            int size=j-i+1;
-            if(size>ans.size()) ans=s.substr(i,size);
-            return dp[i][j]=size;
+    int solve(int st,int end,string &s,vector<vector<int>>& dp){
+        if(st>end) return 0;
+        if(dp[st][end]!=-1) return dp[st][end];
+        if(s[st]==s[end] && isPal(st,end,s)){
+            int size=end-st+1;
+            if(size>ans.size()) ans=s.substr(st,size);
         }
-        if(j-i+1<ans.size()) return 0;
-        int takel=solve(i+1,j,s,dp);
-        int taker=solve(i,j-1,s,dp);
-        return dp[i][j]= max(takel,taker);
+        int take=solve(st+1,end,s,dp);
+        int notTake=solve(st,end-1,s,dp);
+        return dp[st][end]=max(take,notTake);
     }
     string longestPalindrome(string s) {
         int n=s.size();
