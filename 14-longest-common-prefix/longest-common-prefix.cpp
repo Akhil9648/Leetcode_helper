ff6887485746a42d72efb1ed5d254a0860de53e3
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-10 10:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (39.88%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,11 +4,12 @@ public:
         string curr=strs[0];
         int n=strs.size();
         for(int i=1;i<n;i++){
+            int k=curr.size();
             int m=strs[i].size();
             if(m<curr.size()){
                 curr=curr.substr(0,m);
             }
-            for(int j=0;j<min(curr.size(),strs[i].size());j++){
+            for(int j=0;j<min(k,m);j++){
                 if(curr[j]!=strs[i][j]){
                     curr=curr.substr(0,j);
                 }
