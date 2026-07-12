0f63a1813526e1df48203c73010faacdc2db8287
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-12 18:52:21 +0530

    Time: 1 ms (37.31%) | Memory: 17.7 MB (6.68%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    vector<int>dr={0,1,0,-1};
-    vector<int>dc={1,0,-1,0};
+    int dr[4]={0,1,0,-1};
+    int dc[4]={1,0,-1,0};
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
+        queue<vector<int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({i,j});
+                    q.push({i,j,0});
                 }
             }
         }
+        int cnt=0;
         while(!q.empty()){
             auto it=q.front();
-            int r=it.first;
-            int c=it.second;
+            int r=it[0];
+            int c=it[1];
+            int cost=it[2];
+            cnt=max(cnt,cost);
             q.pop();
             for(int k=0;k<4;k++){
                 int nr=r+dr[k];
                 int nc=c+dc[k];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !vis[nr][nc]){
-                    q.push({nr,nc});
-                    vis[nr][nc]=vis[r][c]+1;
+                 if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                    grid[nr][nc]=2;
+                    q.push({nr,nc,cost+1});
                 }
             }
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
+         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
+                if(grid[i][j]==1){
                     return -1;
                 }
-                ans=max(ans,vis[i][j]);
             }
         }
-        return ans;
+        return cnt;
     }
 };
