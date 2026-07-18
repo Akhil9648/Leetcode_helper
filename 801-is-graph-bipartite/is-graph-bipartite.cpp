3da441e0fc06ea88e1c02399ed60b88824e12289
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-18 16:49:46 +0530

    Time: 3 ms (13.02%) | Memory: 17.5 MB (58.66%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,53 +1,21 @@
-// BFS
-class Solution1 {
-public:
-    bool check(int i,int n,vector<vector<int>>& graph,vector<int>& color){
-        queue<int>q;
-        q.push(i);
-        color[i]=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(int it:graph[node]){
-                if(color[it]==-1){
-                    color[it]=!color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node]) return false;
-            }
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>color(n,-1);
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(!check(i,n,graph,color)) return false;
-            }
-        }
-        return true;
-    }
-};
-// DFS
 class Solution {
 public:
-    bool dfs(int i,int col,vector<int>&color,vector<vector<int>>& graph){
-        color[i]=col;
+    bool dfs(int i,int col,vector<int>& colour,vector<vector<int>>& graph){
+        colour[i]=col;
         for(auto it:graph[i]){
-            if(color[it]==-1){
-                if(!dfs(it,!col,color,graph)) return false;
+            if(colour[it]==-1){
+                if(!dfs(it,!col,colour,graph)) return false;
             }
-            else if(color[it]==col) return false;
+            if(colour[it]==col) return false;
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>color(n,-1);
+        vector<int>colour(n,-1);
         for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(!dfs(i,0,color,graph)) return false;
+            if(colour[i]==-1){
+                if(!dfs(i,0,colour,graph)) return false;
             }
         }
         return true;
