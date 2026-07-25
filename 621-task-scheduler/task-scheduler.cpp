2aa3d9ab0f238a5bf985bf522e2464d379df1023
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-26 00:05:33 +0530

    Time: 7 ms (65.40%) | Memory: 38.3 MB (84.37%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<pair<int,int>>pq;
-        vector<int>cnt(26,0),pri(26,0);
+        priority_queue<int>pq;
+        vector<int>cnt(26,0);
         for(auto it:tasks){
             cnt[it-'A']++;
         }
         for(int i=0;i<26;i++){
             if(cnt[i]>0){
-                pq.push({cnt[i],i});
+                pq.push(cnt[i]);
             }
         }
         int ans=0;
+        vector<int>remain;
         while(!pq.empty()){
-            priority_queue<pair<int,int>>cu;
-                for(int i=0;i<26;i++){
-                    pri[i]--;
-                }
-            while(!pq.empty()){
-                auto it=pq.top();
-                int c=it.first;
-                int curr=it.second;
-                if(pri[curr]<0){
-                    pq.pop();
-                    if(c>1) pq.push({c-1,curr});
-                    pri[curr]=n;
-                    break;
-                }
-                else{
-                    cu.push(it);
-                    pq.pop();
-                }
-            }
-            while(!cu.empty()){
-                auto it=cu.top();
-                pq.push(it);
-                cu.pop();
-            }
+            auto it=pq.top();
+            pq.pop();
             ans++;
+            if(it>1) remain.push_back(it-1);
+            int b=n;
+            if(pq.empty() && remain.empty()) break;
+            while(b--){
+                if(pq.empty() && remain.empty()) break;
+                ans++;
+                if(pq.size()>0){
+                    auto k=pq.top();
+                    pq.pop();
+                    if(k>1) remain.push_back(k-1);
+                }
+            }
+            for(auto i:remain) pq.push(i);
+            remain.clear();
         }
         return ans;
     }
