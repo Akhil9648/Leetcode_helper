51bfa1296638d8b14b13e8284732d42efcbf4a89
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-25 19:06:50 +0530

    Time: 15 ms (50.16%) | Memory: 25.4 MB (27.82%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int n;
-    void solve(int i,vector<int>& nums,int target,int &cnt){
+    int k;
+    int solve(int i,vector<int>& nums,int target,vector<vector<int>>& dp){
         if(i>=n){
-            if(target==0) cnt++;
-            return;
+            if(target==0) return 1;
+            return 0;
         }
-        solve(i+1,nums,target-nums[i],cnt);
-        solve(i+1,nums,target+nums[i],cnt);
-        return ;
+        if(abs(target)>k) return 0;
+        if(dp[i][target+k]!=-1) return dp[i][target+k];
+        int plus=solve(i+1,nums,target+nums[i],dp);
+        int minus=solve(i+1,nums,target-nums[i],dp);
+        return dp[i][target+k]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         n=nums.size();
-        int cnt=0;
-        solve(0,nums,target,cnt);
-        return cnt;
+        k = accumulate(nums.begin(), nums.end(), 0);
+        if(abs(target)>k) return 0;
+        vector<vector<int>>dp(n,vector<int>(2*k+1,-1));
+        return solve(0,nums,target,dp);
     }
 };
