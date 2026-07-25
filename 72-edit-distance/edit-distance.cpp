08ab9e10d97ab5da4168d62962c5da26fc752e8c
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-25 23:00:12 +0530

    Time: 8 ms (45.20%) | Memory: 14.5 MB (12.76%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int n,m;
-    int solve(int i,int j,string &w1,string &w2,vector<vector<int>>& dp){
+    int solve(int i,int j,string &word1,string &word2,vector<vector<int>>& dp){
         if(i==n && j==m) return 0;
-        if(i>=n) return m-j;
-        if(j==m) return n-i;
+        if(i==n || j==m){
+            return n-i+m-j;
+        }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(w1[i]==w2[j]){
-            return dp[i][j]=solve(i+1,j+1,w1,w2,dp);
+        if(word1[i]==word2[j]){
+            return dp[i][j]=solve(i+1,j+1,word1,word2,dp);
         }
         return dp[i][j]=1+min({
-            solve(i+1,j+1,w1,w2,dp),
-            solve(i,j+1,w1,w2,dp),
-            solve(i+1,j,w1,w2,dp),
+            solve(i+1,j,word1,word2,dp),
+            solve(i,j+1,word1,word2,dp),
+            solve(i+1,j+1,word1,word2,dp)
         });
     }
     int minDistance(string word1, string word2) {
