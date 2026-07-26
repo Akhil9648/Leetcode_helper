302a370f9dc0724a726a8c43bf4946acfca76057
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-26 19:36:55 +0530

    Time: 7 ms (32.37%) | Memory: 19.3 MB (11.52%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    bool solve(int i,vector<vector<int>>& adj,vector<int>&vis,vector<int>& ans,vector<int>&pathvis){
+    bool solve(int i,vector<vector<int>>& adj,vector<int>& vis,vector<int>& ans,vector<int>& path){
         vis[i]=1;
-        pathvis[i]=1;
+        path[i]=1;
         for(auto it:adj[i]){
             if(!vis[it]){
-                if(solve(it,adj,vis,ans,pathvis)) return true;
+                if(solve(it,adj,vis,ans,path)) return true;
             }
-            else if(pathvis[it]) return true;
+            if(path[it]){
+                return true;
+            }
         }
-        pathvis[i]=0;
+        path[i]=0;
         ans.push_back(i);
         return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
         for(auto it:prerequisites){
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);
         }
         vector<int>vis(numCourses,0),ans,pathvis(numCourses,0);
         for(int i=0;i<numCourses;i++){
@@ -24,7 +26,6 @@ public:
                 if(solve(i,adj,vis,ans,pathvis)) return {};
             }
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
