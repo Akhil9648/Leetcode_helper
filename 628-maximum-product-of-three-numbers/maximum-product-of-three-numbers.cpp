55c4723bf6bb9e193979267efca37ce3945c8692
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-27 11:51:13 +0530

    Time: 11 ms (37.44%) | Memory: 37.1 MB (1.77%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,20 +1,4 @@
-class Solution1 {
-public:
-    int n;
-    int solve(int i,vector<int>& nums,int cnt,int mul){
-        if(cnt==0) return mul;
-        if(i>=n) return -1e9;
-        if(i>=n) return 1;
-        int take=solve(i+1,nums,cnt-1,mul*nums[i]);
-        int notTake=solve(i+1,nums,cnt,mul);
-        return max(take,notTake);
-    }
-    int maximumProduct(vector<int>& nums) {
-        n=nums.size();
-        return solve(0,nums,3,1);
-    }
-};
-class Solution {
+class Solution1{
 public:
     int maximumProduct(vector<int>& nums) {
         int n=nums.size();
@@ -24,5 +8,27 @@ public:
         a=nums[n-1],b=nums[n-2],c=nums[n-3];
         mul=max(mul,a*b*c);
         return mul;
+    }
+};
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>pq1;
+        priority_queue<int>pq2;
+        for(auto it:nums){
+            pq1.push(it);
+            pq2.push(it);
+        }
+        int a=pq1.top();
+        pq1.pop();
+        int b=pq1.top();
+        pq1.pop();
+        int c=pq2.top();
+        pq2.pop();
+        int d=pq2.top();
+        pq2.pop();
+        int e=pq2.top();
+        pq2.pop();
+        return max(a*b*c,c*d*e);
     }
 };
