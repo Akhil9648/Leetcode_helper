0c6d258170af0cab2255f26f4868235406b35551
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-27 12:21:34 +0530

    Time: 15 ms (12.33%) | Memory: 8.4 MB (59.93%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -12,7 +12,7 @@ public:
         return cnt;
     }
 };
-class Solution {
+class Solution2 {
 public:
     int distributeCandies(int n, int limit) {
         int cnt=0;
@@ -23,5 +23,20 @@ public:
             }
         }
         return cnt;
+    }
+};
+class Solution {
+public:
+    int solve(int n,int limit,int child){
+        if(n==0 && child==0) return 1;
+        if(child==0) return 0;
+        int ans=0;
+        for(int i=0;i<=limit;i++){
+            if(n>=i) ans+=solve(n-i,limit,child-1);
+        }
+        return ans;
+    }
+    int distributeCandies(int n, int limit) {
+        return solve(n,limit,3);
     }
 };
