4994fa8edec02cfaba277b01d0ce13f8905fe75e
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-27 12:24:34 +0530

    Time: 7 ms (29.11%) | Memory: 10.8 MB (2.40%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -27,16 +27,18 @@ public:
 };
 class Solution {
 public:
-    int solve(int n,int limit,int child){
+    int solve(int n,int limit,int child,vector<vector<int>>& dp){
         if(n==0 && child==0) return 1;
         if(child==0) return 0;
+        if(dp[n][child]!=-1) return dp[n][child];
         int ans=0;
         for(int i=0;i<=limit;i++){
-            if(n>=i) ans+=solve(n-i,limit,child-1);
+            if(n>=i) ans+=solve(n-i,limit,child-1,dp);
         }
-        return ans;
+        return dp[n][child]=ans;
     }
     int distributeCandies(int n, int limit) {
-        return solve(n,limit,3);
+        vector<vector<int>>dp(n+1,vector<int>(4,-1));
+        return solve(n,limit,3,dp);
     }
 };
