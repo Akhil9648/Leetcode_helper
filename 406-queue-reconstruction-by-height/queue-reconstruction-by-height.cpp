21d72d526f1f341367ea80987534d30cbc794ef2
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-28 16:50:13 +0530

    Time: 13 ms (86.72%) | Memory: 16 MB (59.55%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(), people.end(), [](const vector<int>& a, const vector<int>& b) {
-        if (a[0] == b[0]) return a[1] > b[1];
-        return a[0] < b[0];
+        sort(people.begin(),people.end(),[](const auto &a,const auto &b){
+            if(a[0]==b[0]) return a[1]<b[1];
+            return a[0]<b[0];
         });
         int n=people.size();
-        int pos=0;
         vector<vector<int>>ans(n,vector<int>(2,-1));
         for(int i=0;i<n;i++){
-            int cnt=people[i][1];
+            int count=people[i][1];
+            int val=people[i][0];
+            int a=count;
             for(int j=0;j<n;j++){
-                if (ans[j][0] == -1) {
-                    if (cnt == 0) {
-                        ans[j] = people[i];
-                        break;
-                    }
-                    cnt--; 
+                if(ans[j][0]==-1 && count==0){
+                    ans[j][0]=val;
+                    ans[j][1]=a;
+                    break;
                 }
+                else if(people[i][0]<=ans[j][0] || ans[j][0]==-1) count--;
             }
         }
         return ans;
