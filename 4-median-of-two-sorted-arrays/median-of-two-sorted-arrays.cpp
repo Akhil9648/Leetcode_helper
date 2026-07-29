805e51b43c878a4b3971a08b7170f1fc0201a819
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-29 12:28:47 +0530

    Time: 8 ms (5.89%) | Memory: 96.1 MB (9.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        priority_queue<int,vector<int>,greater<int>>mini;
+        priority_queue<int>maxi;
         int n=nums1.size();
         int m=nums2.size();
-        if(n>m) return findMedianSortedArrays(nums2,nums1);
-        int low=0,high=n;
-        int size=(n+m+1)/2;
-        while(low<=high){
-            int mid1=low+(high-low)/2;
-            int mid2=size-mid1;
-            int l1=INT_MIN,l2=INT_MIN;
-            int r1=INT_MAX,r2=INT_MAX;
-            if(mid1<n) r1=nums1[mid1];
-            if(mid2<m) r2=nums2[mid2];
-            if(mid1>0) l1=nums1[mid1-1];
-            if(mid2>0) l2=nums2[mid2-1];
-            if(l1>r2) high=mid1-1;
-            else if(l2>r1) low=mid1+1;
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                mini.push(nums1[i]);
+                i++;
+            }
             else{
-                if((n+m)%2==0){
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                }
-                else{
-                    return max(l1,l2)/1.0;
-                }
+                mini.push(nums2[j]);
+                j++;
+            }
+            if(mini.size()>maxi.size()){
+                int a=mini.top();
+                mini.pop();
+                maxi.push(a);
             }
         }
-        return 0.0;
+        while(i<n){
+            mini.push(nums1[i]);
+            i++;
+            if(mini.size()>maxi.size()){
+                int a=mini.top();
+                mini.pop();
+                maxi.push(a);
+            }
+        }
+        while(j<m){
+            mini.push(nums2[j]);
+            j++;
+            if(mini.size()>maxi.size()){
+                int a=mini.top();
+                mini.pop();
+                maxi.push(a);
+            }
+        }
+        if((n+m)%2==0){
+            return (maxi.top()+mini.top())/2.0;
+        }
+        return maxi.top()/1.0;
     }
 };
