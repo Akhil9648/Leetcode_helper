bd584d75fad1ff9a98ebadf3e7d5c36c9dc121eb
Akhil Pandey <akhilpandey494@gmail.com>
2026-07-30 10:30:52 +0530

    Time: 4 ms (40.82%) | Memory: 13.9 MB (42.72%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,9 +1,16 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        write=1
         n=len(nums)
-        k=1
         for i in range(1,n):
-            if(nums[i]!=nums[i-1]):
-                nums[k]=nums[i]
-                k=k+1
-        return k
+            if nums[i]!=nums[i-1]:
+                nums[write]=nums[i]
+                write+=1
+        return write
+        
