4f740be0e7fb61a6536465980401b891d71a29a3
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-17 12:28:44 +0530

    Time: 2 ms (30.70%) | Memory: 17.3 MB (16.70%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int dr[4]={0,1,0,-1};
-        int dc[4]={1,0,-1,0};
+        queue<pair<int,int>>q;
+        int dr[6]={0,1,0,-1};
+        int dc[6]={1,0,-1,0};
         int n=grid.size();
         int m=grid[0].size();
-        queue<vector<int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j,0});
-                }
+                if(grid[i][j]==2) q.push({i,j});
             }
         }
-        int cnt=0;
+        int ans=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         while(!q.empty()){
             auto it=q.front();
-            int r=it[0];
-            int c=it[1];
-            int cost=it[2];
-            cnt=max(cnt,cost);
+            int r=it.first;
+            int c=it.second;
             q.pop();
-            for(int k=0;k<4;k++){
-                int nr=r+dr[k];
-                int nc=c+dc[k];
-                 if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m  && vis[nr][nc]==0 && grid[nr][nc]==1){
+                    vis[nr][nc]=vis[r][c]+1;
                     grid[nr][nc]=2;
-                    q.push({nr,nc,cost+1});
+                    q.push({nr,nc});
                 }
             }
         }
-         for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
+                if(grid[i][j]==1) return -1;
+                ans=max(ans,vis[i][j]);
             }
         }
-        return cnt;
+        return ans;
     }
 };
