f162e6d114c8686a813f9ad751867b437794295c
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-17 10:18:53 +0530

    Time: 259 ms (5.42%) | Memory: 10.3 MB (23.42%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        string a;
+        for(auto it:s){
+            if((it>='a' && it<='z') || (it>='0' && it<='9')) a.push_back(it);
+            cout<<it<<" ";
+        }
+        s=a;
+        reverse(a.begin(),a.end());
+        return a==s;
+    }
+};
