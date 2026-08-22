019b1d3d4054bb0937b20b2feeb4c4f973eaaa6d
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-22 18:52:11 +0530

    Time: 85 ms (36.73%) | Memory: 89 MB (25.41%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,13 +5,12 @@ public:
         int n=nums.size();
         for(auto it:nums) st.insert(it);
         int maxi=0;
-        for(auto it:nums){
+        for(auto &it:st){
             int a=it;
             if(st.count(a-1)) continue;
             int cnt=0;
             while(st.count(a)){
                 cnt++;
-                st.erase(a);
                 a++;
             }
             maxi=max(maxi,cnt);
