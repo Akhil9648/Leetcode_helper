dd8749677a547c724ca9053fe23df4efce6c7ca8
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-23 02:58:27 +0530

    Time: 7 ms (61.46%) | Memory: 11.2 MB (94.68%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,68 +1,28 @@
-class Solution1 {
-public:
-bool check(unordered_map<char,int>&curr,unordered_map<char,int>& req){
-        for(auto it:req){
-            char c=it.first;
-            if(!curr.count(c) || curr[c]<it.second) return false;
-        }
-        return true;
-    }
-    string minWindow(string s, string t) {
-        unordered_map<char,int>curr,req;
-        for(auto it:t){
-            req[it]++;
-        }
-        int n=s.size();
-        int i=0,j=0;
-        string ans="";
-        int cnt=INT_MAX;
-        while(j<n){
-            curr[s[j]]++;
-            bool chk=false;
-            if(curr.size()>=req.size()){
-                chk=check(curr,req);
-            }
-            while(chk && i<n){
-                if(cnt>(j-i+1)){
-                    cnt=j-i+1;
-                    ans=s.substr(i,cnt);
-                    if(ans.size()==1) return ans;
-                }
-                curr[s[i]]--;
-                if(req.count(s[i])){
-                    if(curr[s[i]]<req[s[i]]) chk=false;
-                }
-                if(curr[s[i]]==0) curr.erase(s[i]);
-                i++;
-            }
-            j++;
-        }
-        return ans;
-    }
-};
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>mp;
-        int cnt=0;
-        for(char c:t) mp[c]++;
+        unordered_map<int,int>mp;
+        for(auto it:t){
+            mp[it]++;
+        }
         int n=s.size();
-        int ans=INT_MAX,st=n;
-        int i=0,j=0;
+        int m=t.size();
+        int i=0,j=0,st=0,l=n+1,cnt=0;
         while(j<n){
             if(mp[s[j]]>0) cnt++;
-            while(cnt==t.size()){
-                if(ans>(j-i+1)){
-                    ans=j-i+1;
+            mp[s[j]]--;
+            while(cnt==m){
+                if(j-i+1<l){
+                    l=j-i+1;
                     st=i;
                 }
                 mp[s[i]]++;
                 if(mp[s[i]]>0) cnt--;
                 i++;
             }
-            mp[s[j]]--;
             j++;
         }
-        return s.substr(st,ans);
+        if(l==n+1) return "";
+        return s.substr(st,l);
     }
 };
