b9e7707bccee0a8617147efada87c0a489fc421e
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-24 18:49:23 +0530

    Time: 3 ms (68.54%) | Memory: 11.7 MB (5.46%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    unordered_map<string,string>mp;
+    string encode(string longUrl) {
+        string s="";
+        int n=longUrl.size();
+        s+=(to_string(n));
+        int i=n-1;
+        while(i>=0){
+            if(longUrl[i]=='/') break;
+            i--;
+        }
+        i++;
+        int k=i;
+        int vowel=0,cons=0,sym=0;
+        while(i<n){
+            if(longUrl[i]=='a' || longUrl[i]=='e' || longUrl[i]=='i' || longUrl[i]=='o' || longUrl[i]=='u') vowel++;
+            else if(longUrl[i]>='a' && longUrl[i]<='z') cons++;
+            else sym++;
+            i++;
+        }
+        s+=(to_string(vowel)+to_string(cons)+to_string(sym));
+        s+=(longUrl[k]+longUrl[n-1]);
+        // string shortUrl=longUrl.substr(0,k+1);
+        // shortUrl+=(s);
+        mp[s]=longUrl;
+        return s;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return mp[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
