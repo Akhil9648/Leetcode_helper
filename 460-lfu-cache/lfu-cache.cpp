737f8b10ce6508ebb9c8c78a29deaef37082952d
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-30 19:41:02 +0530

    Time: 121 ms (41.64%) | Memory: 198.5 MB (17.60%) - LeetSync

--- 460-lfu-cache/lfu-cache.cpp
+++ 460-lfu-cache/lfu-cache.cpp
@@ -1,85 +1,66 @@
-struct Node {
-    int key, value, count;
-    Node* next, *prev;
-
-    Node(int k, int v, int c) {
-        key = k;
-        value = v;
-        count = c;
+struct Node{
+    int key,value,count;
+    Node(int k,int v,int c){
+        key=k;
+        value=v;
+        count=c;
     }
 };
-
 class LFUCache {
 public:
-    int cap, cnt = 1;
-
-    unordered_map<int, list<Node*>> cache;
-    unordered_map<int, list<Node*>::iterator> mp;
-
+    int cap,count=1;
+    unordered_map<int, list<int>> cache; 
+    unordered_map<int, Node*> mp;
+    unordered_map<int, list<int>::iterator> pos;
     LFUCache(int capacity) {
-        cap = capacity;
+        cap=capacity;
     }
-
+    
     int get(int key) {
-        if (mp.count(key)) {
-
-            auto node = *mp[key];
-
-            int value = node->value;
-            int count = node->count;
-
-            cache[count].erase(mp[key]);
-            auto it = cache[count + 1].insert(
-                cache[count + 1].end(), node
-            );
-            mp[key] = it;
-
-            node->count++;
-            if (cache[count].size() == 0 && cnt == count)
-                cnt++;
-
-            return value;
+        if(mp.count(key)){
+            Node* curr=mp[key];
+            int lvl=curr->count;
+            cache[lvl].erase(pos[key]);
+            cache[lvl+1].push_back(key);
+            pos[key] = --cache[lvl+1].end(); 
+            curr->count++;
+            if(cache[count].size()==0 && lvl == count) count++;
+            return curr->value;
         }
-
         return -1;
     }
-
+    
     void put(int key, int value) {
-
-        if (mp.count(key)) {
-
-            auto node = *mp[key];
-            int count = node->count;
-
-            cache[count].erase(mp[key]);
-            auto it = cache[count + 1].insert(
-                cache[count + 1].end(), node
-            );
-            mp[key] = it;
-
-            node->count++;
-            node->value = value;
-
-            if (cache[count].size() == 0 && cnt == count)
-                cnt++;
-
+        if (cap <= 0) return;
+        if(mp.count(key)){
+            Node* curr=mp[key];
+            int lvl=curr->count;
+            cache[lvl].erase(pos[key]);
+            cache[lvl+1].push_back(key);
+            pos[key] = --cache[lvl+1].end(); 
+            if(cache[count].size()==0 && lvl == count) count++;
+            curr->count++;
+            curr->value=value;
             return;
         }
-        if (cap == 0)
-            return;
+        if(mp.size()==cap){
+            int curr_key = cache[count].front();
+            cache[count].pop_front();
+            mp.erase(curr_key);
+            pos.erase(curr_key);
 
-        if (mp.size() == cap) {
-            auto node = cache[cnt].front();
-            cache[cnt].pop_front();
-            mp.erase(node->key);
-            delete node;
         }
-
-        Node* curr = new Node(key, value, 1);
-        auto it = cache[1].insert(
-            cache[1].end(), curr
-        );
-        mp[key] = it;
-        cnt = 1;
+        cache[1].push_back(key);
+        pos[key] = --cache[1].end(); 
+        Node* curr=new Node(key,value,1);
+        count=1;
+        mp[key]=curr;
     }
 };
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
