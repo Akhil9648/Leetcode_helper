279f9a75c4b58271b913b5e288d07f7bb6c18a7a
Akhil Pandey <akhilpandey494@gmail.com>
2026-08-31 23:51:41 +0530

    Time: 8 ms (26.86%) | Memory: 124.6 MB (81.63%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,24 +11,25 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        ListNode* temp=head;
-        int prev=-1;
-        vector<int>arr;
-        int b=0;
-        while(temp->next){
-            int a=temp->val;
-            if(prev!=-1 && (a>prev && a>temp->next->val || a<prev && a<temp->next->val)) arr.push_back(b);
-            prev=a;
-            temp=temp->next;
-            b++;
+        int cnt=1;
+        int fir=-1,last=-1;
+        int mini=1e9,maxi=0;
+        ListNode* prev=head,*ptr=head->next;
+        while(ptr->next){
+            if((ptr->val>prev->val && ptr->val>ptr->next->val) || (ptr->val<prev->val && ptr->val<ptr->next->val)){
+                if(fir==-1) fir=cnt;
+                else{
+                    if(last!=-1) mini=min(mini,cnt-last);
+                    last=cnt;
+                    mini=min(mini,last-fir);
+                    maxi=max(maxi,last-fir);
+                }
+            }
+            prev=ptr;
+            ptr=ptr->next;
+            cnt++;
         }
-        int mini=INT_MAX,maxi=INT_MIN;
-        if(arr.size()<2) return {-1,-1};
-        int n=arr.size();
-        for(int i=1;i<n;i++){
-            int a=arr[i]-arr[i-1];
-            mini=min(mini,a);
-        }
-        return {mini,arr[n-1]-arr[0]};
+        if(mini==1e9 || maxi==0) return {-1,-1};
+        return {mini,maxi};
     }
 };
